e8622b2927d9257fe7dabc31bb36800cc5996235
2805Dhyani <narendra.dasuju@gmail.com>
2026-08-17 19:27:32 +0530

    Add files via upload

---
+++ session2.py
@@ -0,0 +1,14 @@
+print("welcome to codingal.")
+
+print("Today we are going to learn python.")
+
+print(28)
+
+print("Hello everyone \n")
+
+print("I am Dhyani " , 8)
+
+print("Thank you" , end="*")
+
+
+
---
+++ session3.py
@@ -0,0 +1,12 @@
+x = 13
+y = "Dhyani"
+
+print(x)
+print(y)
+
+codingal = "codingal"
+print (codingal)
+
+name = input("enter your name ")
+
+print("\n welcome" , name ,  "\n to codingal")
